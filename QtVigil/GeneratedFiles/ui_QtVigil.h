ce881db14740dace9094df28a89ecfdb91e9a28d
khaledsalem999 <khaledsalem999@gmail.com>
2017-03-24 22:35:12 +0200

    Added feed screen

--- QtVigil/GeneratedFiles/ui_QtVigil.h
+++ QtVigil/GeneratedFiles/ui_QtVigil.h
@@ -13,9 +13,12 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -25,25 +28,79 @@ QT_BEGIN_NAMESPACE
 class Ui_QtVigilClass
 {
 public:
+    QWidget *centralWidget;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *QtVigilClass)
     {
         if (QtVigilClass->objectName().isEmpty())
             QtVigilClass->setObjectName(QStringLiteral("QtVigilClass"));
-        QtVigilClass->resize(600, 400);
+        QtVigilClass->resize(951, 578);
+        centralWidget = new QWidget(QtVigilClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        horizontalLayoutWidget = new QWidget(centralWidget);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(0, 419, 951, 91));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton_2 = new QPushButton(horizontalLayoutWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
+        pushButton_2->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(pushButton_2);
+
+        pushButton = new QPushButton(horizontalLayoutWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        sizePolicy.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(pushButton);
+
+        label_2 = new QLabel(horizontalLayoutWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setLayoutDirection(Qt::LeftToRight);
+        label_2->setAutoFillBackground(false);
+        label_2->setAlignment(Qt::AlignCenter);
+        label_2->setWordWrap(false);
+
+        horizontalLayout->addWidget(label_2);
+
+        label_3 = new QLabel(horizontalLayoutWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(label_3);
+
+        label = new QLabel(horizontalLayoutWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(label);
+
+        QtVigilClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(QtVigilClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 951, 26));
         QtVigilClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(QtVigilClass);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        QtVigilClass->addToolBar(mainToolBar);
-        centralWidget = new QWidget(QtVigilClass);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        QtVigilClass->setCentralWidget(centralWidget);
+        QtVigilClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(QtVigilClass);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         QtVigilClass->setStatusBar(statusBar);
@@ -56,6 +113,11 @@ public:
     void retranslateUi(QMainWindow *QtVigilClass)
     {
         QtVigilClass->setWindowTitle(QApplication::translate("QtVigilClass", "QtVigil", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("QtVigilClass", "PushButton", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("QtVigilClass", "PushButton", Q_NULLPTR));
+        label_2->setText(QApplication::translate("QtVigilClass", "TextLabel", Q_NULLPTR));
+        label_3->setText(QApplication::translate("QtVigilClass", "TextLabel", Q_NULLPTR));
+        label->setText(QApplication::translate("QtVigilClass", "TextLabel", Q_NULLPTR));
     } // retranslateUi
 
 };
