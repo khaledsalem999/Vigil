53ba9beb4dd84eceeec9a6793e09a8a55d130be5
hyhae <hussein132942@miuegypt.edu.eg>
2017-05-03 11:27:05 +0200

    removed a couple of functions in SVMTRAINER

--- QtVigil/SVMTrainer.cpp
+++ QtVigil/SVMTrainer.cpp
@@ -56,27 +56,6 @@ void SVMTrainer::initializeSVM(QString posTemp, QString negTemp) {
 	//test_it(Size(320, 240)); // change with your parameters
 }
 
-void SVMTrainer::get_svm_detector(const Ptr<SVM>& svm, vector< float > & hog_detector)
-{
-	// get the support vectors
-	Mat sv = svm->getSupportVectors();
-	const int sv_total = sv.rows;
-	// get the decision function
-	Mat alpha, svidx;
-	double rho = svm->getDecisionFunction(0, alpha, svidx);
-
-	CV_Assert(alpha.total() == 1 && svidx.total() == 1 && sv_total == 1);
-	CV_Assert((alpha.type() == CV_64F && alpha.at<double>(0) == 1.) ||
-		(alpha.type() == CV_32F && alpha.at<float>(0) == 1.f));
-	CV_Assert(sv.type() == CV_32F);
-	hog_detector.clear();
-
-	hog_detector.resize(sv.cols + 1);
-	memcpy(&hog_detector[0], sv.ptr(), sv.cols * sizeof(hog_detector[0]));
-	hog_detector[sv.cols] = (float)-rho;
-}
-
-
 /*
 * Convert training/testing set to be used by OpenCV Machine Learning algorithms.
 * TrainData is a matrix of size (#samples x max(#cols,#rows) per samples), in 32FC1.
--- QtVigil/SVMTrainer.h
+++ QtVigil/SVMTrainer.h
@@ -14,7 +14,6 @@ public:
 	SVMTrainer(QObject *parent);
 	~SVMTrainer();
 	void initializeSVM(QString posTemp, QString negTemp);
-	void get_svm_detector(const Ptr<SVM>& svm, vector<float>& hog_detector);
 	void convert_to_ml(const std::vector<cv::Mat>& train_samples, cv::Mat & trainData);
 	void load_images(const string & filename, vector<Mat>& img_lst);
 	void sample_neg(const vector<Mat>& full_neg_lst, vector<Mat>& neg_lst, const Size & size);
